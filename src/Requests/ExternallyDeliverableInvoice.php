b54a6e07bc4b930edfc8ad1a57592ef84f6bb0f9
Helge Sverre <helgesverre@guilty.no>
2019-03-12 19:10:29 +0100

    Class to make building ExternallyDeliverableInvoiceDeliveredRequest easier

---
+++ src/Requests/ExternallyDeliverableInvoice.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Guilty\Poweroffice\Requests;
+
+
+class ExternallyDeliverableInvoiceDeliveredRequest
+{
+    protected $externallyDeliverableInvoiceId;
+    protected $fileBase64;
+    protected $fileName;
+    /**
+     * @var null
+     */
+    protected $comment;
+
+    public function __construct($externallyDeliverableInvoiceId, $comment = null)
+    {
+        $this->externallyDeliverableInvoiceId = $externallyDeliverableInvoiceId;
+        $this->comment = $comment;
+    }
+
+    public function withComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function withFile($filePath, $filename = null)
+    {
+        if (!file_exists($filePath)) {
+            throw new \InvalidArgumentException("File does not exist");
+        }
+
+        if (!$filename) {
+            $filename = basename($filePath);
+        }
+
+        $data = file_get_contents($filePath);
+        $this->fileBase64 = base64_encode($data);
+        $this->fileName = $filename;
+
+        return $this;
+    }
+
+    public function toArray()
+    {
+        return [
+            "Comment" => $this->comment,
+            "ExternallyDeliverableInvoiceId" => $this->externallyDeliverableInvoiceId,
+            "File" => [
+                "Base64EncodedData" => $this->fileBase64,
+                "FileName" => $this->fileName,
+            ],
+        ];
+    }
+}
